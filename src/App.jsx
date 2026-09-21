d37339ec15b95eaf10e922d51c0fe061a5522de8
Manuel Raphael De Guzman <leunamednamzug@gmail.com>
2026-09-21 23:33:55 +0800

    1.1: course information step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 const App = () => {
+  const course = 'CSIT340 - Industry Elective 1'
+
   return (
     <div>
-      <p>Hello world</p>
+      <h1>{course}</h1>
+
+      <p>CSIT321 — 3 units</p>
+      <p>IT365 — 3 units</p>
+      <p>IT327 — 3 units</p>
     </div>
   )
 }
